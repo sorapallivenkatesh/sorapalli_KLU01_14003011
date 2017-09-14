dea1c7cce12aaebbbe3df202a1cded14277fb2e6
Sorapalli Venkatesh <sorapalli.venkatesh@gmail.com>
2017-09-14 13:51:25 +0530

    EPAM Files

---
+++ Main.java
@@ -0,0 +1,25 @@
+package src;
+//import Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+                 StudentGroup sg=new StudentGroup(5);
+                 
+                Student stu1=new Student(3011,"Venkatesh",1996-10-17,50.0);
+                Student stu2=null;
+                stu2.setId(11);
+                stu2.setFullName("Venki");
+                stu2.setBirthDate();
+                stu2.setAvgMark(45.0);
+                
+               
+                
+                
+	}
+
+}
